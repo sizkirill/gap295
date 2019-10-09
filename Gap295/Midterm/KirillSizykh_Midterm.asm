3fc18add5347128b95a6dda6ca2801a616e6dde2
sizkirill <megatpyn@gmail.com>
2019-10-09 16:25:14 +0300

    Midterm finished

--- Gap295/Midterm/KirillSizykh_Midterm.asm
+++ Gap295/Midterm/KirillSizykh_Midterm.asm
@@ -16,15 +16,8 @@ extern time:NEAR
 extern system:NEAR
 
 .data
-    testString db '%d', 0ah, 0
-    player dd 0, 0
-
-    turncounter dd 0
-
-    playerPosTestString db 'Player pos: x = %d, y = %d', 0ah, 0
-
+    playerPosString db 'Player pos: x = %d, y = %d', 0ah, 0
     clearScreen db 'cls', 0
-    fakeClearScreen db 0ah,0ah,0ah,0ah,0ah,0ah,0ah,0ah,0ah,0ah,0ah,0ah,0ah,0ah,0ah,0ah,0ah,0ah,0ah,0ah,0ah,0ah,0ah,0ah,0ah,0ah,0ah,0ah,0ah,0ah,0ah,0ah,0ah,0
 
     loseString db 'Oh nO! You lost!', 0ah, 0
     winString db 'Congrats! You win!', 0ah, 0
@@ -39,268 +32,13 @@ extern system:NEAR
     MAP_OFFSET EQU MAP_WIDTH * MAP_HEIGHT + MAP_HEIGHT + 1
     ENEMY_OFFSET EQU MAP_OFFSET + ENEMY_COUNT * 8
 	TRAP_OFFSET EQU ENEMY_OFFSET + TRAP_COUNT * 8
-	EXIT_OFFSET EQU TRAP_OFFSET + 8
+    PLAYER_OFFSET EQU TRAP_OFFSET + 8
+	EXIT_OFFSET EQU PLAYER_OFFSET + 8
 
-InitPlayer MACRO
-    call rand
-    xor edx, edx
-    idiv MAP_WIDTH
-    mov offset player, edx
-    call rand
-    xor edx, edx
-    idiv MAP_HEIGHT
-    mov offset player + 4, edx
-ENDM
-
-InitializeMap MACRO
-        mov edi, ebp
-        sub edi, MAP_WIDTH * MAP_HEIGHT + MAP_HEIGHT
-    InitLoop:
-        mov ecx, MAP_WIDTH
-    InitRow:
-        mov byte ptr [edi-1], 2eh
-        inc edi
-        loop InitRow
-        mov byte ptr [edi-1], 0ah
-        inc edi
-        cmp edi, ebp
-        jl InitLoop
-        mov byte ptr [edi-1], 0h
-ENDM
-
-InitExit MACRO
-		mov edi, ebp
-        sub edi, EXIT_OFFSET
-        mov ebx, 0
-    InitExitLoop:
-        call rand
-        mov ecx, MAP_WIDTH
-        xor edx, edx
-        idiv ecx
-        mov [edi], edx
-        call rand
-        mov ecx, MAP_HEIGHT
-        xor edx, edx
-        idiv ecx
-        mov [edi+4], edx
-
-        ;;;; CHECKING IF SPACE IS EMPTY
+PushMapOffset MACRO
         mov esi, ebp
-        sub esi, MAP_WIDTH * MAP_HEIGHT + MAP_HEIGHT + 1
-        add esi, [edi]
-        mov ecx, MAP_WIDTH + 1
-        imul ecx, [edi+4]
-        add esi, ecx
-        cmp [esi], byte ptr 2eh
-        jne InitExitLoop
-        mov [esi], byte ptr 57h
-ENDM
-
-InitTraps MACRO
-		mov edi, ebp
-        sub edi, TRAP_OFFSET
-        mov ebx, 0
-    InitTrapLoop:
-        call rand
-        mov ecx, MAP_WIDTH
-        xor edx, edx
-        idiv ecx
-        mov [edi], edx
-        call rand
-        mov ecx, MAP_HEIGHT
-        xor edx, edx
-        idiv ecx
-        mov [edi+4], edx
-
-        ;;;; CHECKING IF SPACE IS EMPTY
-        mov esi, ebp
-        sub esi, MAP_WIDTH * MAP_HEIGHT + MAP_HEIGHT + 1
-        add esi, [edi]
-        mov ecx, MAP_WIDTH + 1
-        imul ecx, [edi+4]
-        add esi, ecx
-        cmp [esi], byte ptr 2eh
-        jne InitTrapLoop
-        mov [esi], byte ptr 54h
-        ;;;;
-
-        add edi, 8
-        inc ebx
-        cmp ebx, TRAP_COUNT
-        jl InitTrapLoop
-ENDM
-
-InitializeEnemies MACRO
-        mov edi, ebp
-        sub edi, ENEMY_OFFSET
-        mov ebx, 0
-    InitEnemyLoop:
-        call rand
-        mov ecx, MAP_WIDTH
-        xor edx, edx
-        idiv ecx
-        mov [edi], edx
-        call rand
-        mov ecx, MAP_HEIGHT
-        xor edx, edx
-        idiv ecx
-        mov [edi+4], edx
-
-        ;;;; CHECKING IF SPACE IS EMPTY
-        mov esi, ebp
-        sub esi, MAP_WIDTH * MAP_HEIGHT + MAP_HEIGHT + 1
-        add esi, [edi]
-        mov ecx, MAP_WIDTH + 1
-        imul ecx, [edi+4]
-        add esi, ecx
-        cmp [esi], byte ptr 2eh
-        jne InitEnemyLoop
-        mov [esi], byte ptr 45h
-        ;;;;
-
-        add edi, 8
-        inc ebx
-        cmp ebx, ENEMY_COUNT
-        jl InitEnemyLoop
-ENDM
-
-ClearScreen MACRO
-        push offset clearScreen
-        call system
-        add esp, 4
-ENDM
-
-FakeClearScreen MACRO
-        push offset fakeClearScreen
-        call printf
-        add esp, 4
-ENDM
-
-PrintMap MACRO
-        ;push offset clearScreen
-        ;call system
-        ;add esp,4
-
-        ClearScreen
-
-        push [edi+4]
-        push [edi]
-        push offset playerPosTestString
-        call printf
-        add esp, 12
-
-        mov esi, ebp
-        sub esi, MAP_WIDTH * MAP_HEIGHT + MAP_HEIGHT + 1
+        sub esi, MAP_OFFSET
         push esi
-        call printf
-        add esp, 4
-ENDM
-
-UpdateEnemies MACRO
-        ;int 3
-
-        ; Enemies take turns every other player's turn
-        mov eax, ebx
-        xor edx, edx
-        mov ecx, 2
-        push ebx
-        idiv ecx
-        cmp edx, 0
-        je EnemyUpdateExit
-
-        mov ecx, ebp
-        sub ecx, MAP_OFFSET
-
-        mov esi, ebp
-        ; Although player offset should always be in edi, let's make sure of it
-        mov edi, offset player
-        sub esi, ENEMY_OFFSET
-    EnemyUpdateLoop:
-        push eax
-
-        ; Replacing enemy with empty space
-        mov eax, ebp
-        sub eax, MAP_WIDTH * MAP_HEIGHT + MAP_HEIGHT + 1
-        add eax, [esi]
-        mov edx, MAP_WIDTH + 1
-        imul edx, [esi+4]
-        add eax, edx
-        mov [eax], byte ptr 2eh
-        ;;
-        pop eax
-
-        ; storing current X of enemy
-        push dword ptr [esi]
-
-        mov eax, [esi]
-        sub eax, [edi]
-        je CompareY
-        cmp eax, 0
-        jg IncX
-        inc dword ptr [esi]
-        jmp CompareY
-
-    IncX:
-        dec dword ptr [esi]
-        jmp CompareY
-
-    CompareY:
-        add esi, 4
-        add edi, 4
-        ; Storing current Y of enemy
-        push dword ptr [esi]
-        mov eax, [esi]
-        sub eax, [edi]
-        je CheckPos
-        cmp eax, 0
-        jg IncY
-        inc dword ptr [esi]
-        jmp CheckPos
-
-    IncY:
-        dec dword ptr [esi]
-        jmp CheckPos
-
-    CheckPos:
-        ;;;; CHECKING IF SPACE IS EMPTY
-        mov eax, ebp
-        sub eax, MAP_WIDTH * MAP_HEIGHT + MAP_HEIGHT + 1
-        add eax, [esi-4]
-        mov edx, MAP_WIDTH + 1
-        imul edx, [esi]
-        add eax, edx
-        cmp [eax], byte ptr 2eh
-        jne RollBack
-        mov [eax], byte ptr 45h
-        jmp RestoreStack
-        ;;;;
-    
-    RollBack:
-        ;int 3;
-        mov eax, ebp
-        sub eax, MAP_WIDTH * MAP_HEIGHT + MAP_HEIGHT + 1
-        pop ebx
-        mov [esi], ebx
-        mov edx, MAP_WIDTH + 1
-        imul edx, ebx
-        add eax, edx
-        pop ebx
-        mov [esi-4], ebx
-        add eax, ebx
-        mov [eax], byte ptr 45h
-        jmp NextIter
-
-    RestoreStack:
-        ;Restoring stack before next iteration
-        add esp, 8
-    NextIter:
-        add esi, 4
-        sub edi, 4
-        cmp esi, ecx
-        jl EnemyUpdateLoop
-
-    EnemyUpdateExit:
-        pop ebx
 ENDM
 
 .code
@@ -308,13 +46,16 @@ ENDM
 main proc C
         push ebp
         mov ebp, esp
+
+        ; Allocating memory that we're going to use
+        sub esp, EXIT_OFFSET
+
+        ; Saving registers
         push edi
         push esi
+        push ebx
 
-        ;sub esp, MAP_WIDTH * MAP_HEIGHT + MAP_HEIGHT + 1
-        ;sub esp, ENEMY_COUNT * 8
-		sub esp, EXIT_OFFSET
-
+        ; Init rand
         push 0
         call time
         add esp, 4
@@ -322,41 +63,100 @@ main proc C
         call srand
         add esp, 4
 
-        InitializeMap
-
-        call rand
+        ; Init Map
+        mov edi, ebp
+        sub edi, MAP_WIDTH * MAP_HEIGHT + MAP_HEIGHT
+    InitMapLoop:
         mov ecx, MAP_WIDTH
-        xor edx, edx
-        idiv ecx
-        mov edi, offset player
-        mov [edi], edx
-        call rand
-        mov ecx, MAP_HEIGHT
-        xor edx, edx
-        idiv ecx
-        mov [edi+4], edx
+    InitMapRow:
+        mov byte ptr [edi-1], 2eh
+        inc edi
+        ; while ecx > 0 filling the row with '.'
+        loop InitMapRow
+        ; Endline at the end of the row
+        mov byte ptr [edi-1], 0ah
+        inc edi
+        cmp edi, ebp
+        jl InitMapLoop
+        ; Null terminator at the end of the map
+        mov byte ptr [edi-1], 0h
+
+        ; Init Player
+        PushMapOffset
 
         mov esi, ebp
-        sub esi, MAP_WIDTH * MAP_HEIGHT + MAP_HEIGHT + 1
-        add esi, [edi]
-        mov ecx, MAP_WIDTH + 1
-        imul ecx, [edi+4]
-        add esi, ecx
+        sub esi, PLAYER_OFFSET
+        push esi
 
-        mov byte ptr [esi], 50h
+        push 1
+        push 50h
+        call Init
+        ; End init player
 
-        InitializeEnemies
-		InitTraps
-		InitExit
+        ; Init Enemies
+        PushMapOffset
 
-        ; Turn counter
-        mov edi, offset player
+        mov esi, ebp
+        sub esi, ENEMY_OFFSET
+        push esi
+
+        push ENEMY_COUNT
+        push 45h
+        call Init
+        ; End init enemies
+
+        ; Init Traps
+        PushMapOffset
+
+        mov esi, ebp
+        sub esi, TRAP_OFFSET
+        push esi
+
+        push TRAP_COUNT
+        push 54h
+        call Init
+        ; End init traps
+
+        ; Init exit
+        PushMapOffset
+
+        mov esi, ebp
+        sub esi, EXIT_OFFSET
+        push esi
+
+        push 1
+        push 57h
+        call Init
+        ; End init exit
+
+        ; Init Turn counter (enemies take turns every other turn)
         mov ebx, 0
+        ; For the rest of the game I am trying to keep player coords offset in the EDX register
+        mov edi, ebp
+        sub edi, PLAYER_OFFSET
     MainLoop:
         inc ebx
         
-        PrintMap
+        ; Clear screen
+        push offset clearScreen
+        call system
+        add esp, 4
 
+        ; Print player's position
+        push [edi+4]
+        push [edi]
+        push offset playerPosString
+        call printf
+        add esp, 12
+
+        ; Print map
+        mov esi, ebp
+        sub esi, MAP_WIDTH * MAP_HEIGHT + MAP_HEIGHT + 1
+        push esi
+        call printf
+        add esp, 4
+
+        ; Put empty space on player's position
         mov esi, ebp
         sub esi, MAP_WIDTH * MAP_HEIGHT + MAP_HEIGHT + 1
         add esi, [edi]
@@ -403,23 +203,135 @@ main proc C
         jmp Quit
 
     Update:
-        UpdateEnemies
+        ; Start Update Enemies
+        
+        ; Enemies take turns every other player's turn (First turn of the game is skipped)
+        mov eax, ebx
+        xor edx, edx
+        mov ecx, 2
 
+        ; We're gonna use ebx later, so save it for now (main loop counts turns in ebx)
+        push ebx
+        
+        ; Counting if enemies are supposed to have turn
+        idiv ecx
+        cmp edx, 0
+        jne EnemyUpdateExit
+
+        mov ecx, ebp
+        sub ecx, MAP_OFFSET
+
+        ; Although player offset should always be in edi, let's make sure of it
+        mov edi, ebp
+        sub edi, PLAYER_OFFSET
+        ; ESI stores pointer to the map
+        mov esi, ebp
+        sub esi, ENEMY_OFFSET
+    EnemyUpdateLoop:
+        ; Replacing enemy with empty space
+        mov eax, ebp
+        sub eax, MAP_WIDTH * MAP_HEIGHT + MAP_HEIGHT + 1
+        add eax, [esi]
+        mov edx, MAP_WIDTH + 1
+        imul edx, [esi+4]
+        add eax, edx
+        mov [eax], byte ptr 2eh
+
+        ; storing current X of enemy
+        push dword ptr [esi]
+
+        ; Checking where should the enemy go
+        mov eax, [esi]
+        sub eax, [edi]
+        je CompareY
+        cmp eax, 0
+        jg IncX
+        inc dword ptr [esi]
+        jmp CompareY
+
+    IncX:
+        dec dword ptr [esi]
+        jmp CompareY
+
+    CompareY:
+        add esi, 4
+        add edi, 4
+        ; Storing current Y of enemy
+        push dword ptr [esi]
+        mov eax, [esi]
+        sub eax, [edi]
+        je CheckPos
+        cmp eax, 0
+        jg IncY
+        inc dword ptr [esi]
+        jmp CheckPos
+
+    IncY:
+        dec dword ptr [esi]
+        jmp CheckPos
+
+        ; Checking if the position enemy wants to go to is empty
+    CheckPos:
+        mov eax, ebp
+        sub eax, MAP_WIDTH * MAP_HEIGHT + MAP_HEIGHT + 1
+        add eax, [esi-4]
+        mov edx, MAP_WIDTH + 1
+        imul edx, [esi]
+        add eax, edx
+        cmp [eax], byte ptr 2eh
+        ; If position is occupied we're rolling back to our saved position
+        jne RollBack
+        mov [eax], byte ptr 45h
+        ; else we need to restore stack
+        jmp RestoreStack
+    
+    RollBack:
+        mov eax, ebp
+        sub eax, MAP_WIDTH * MAP_HEIGHT + MAP_HEIGHT + 1
+        pop ebx
+        mov [esi], ebx
+        mov edx, MAP_WIDTH + 1
+        imul edx, ebx
+        add eax, edx
+        pop ebx
+        mov [esi-4], ebx
+        add eax, ebx
+        mov [eax], byte ptr 45h
+        jmp NextIter
+
+    RestoreStack:
+        ;Restoring stack before next iteration
+        add esp, 8
+    NextIter:
+        add esi, 4
+        sub edi, 4
+        cmp esi, ecx
+        jl EnemyUpdateLoop
+
+    EnemyUpdateExit:
+        pop ebx
+        ; End of Enemy update
+
+        ; Updating player
         mov esi, ebp
         sub esi, MAP_WIDTH * MAP_HEIGHT + MAP_HEIGHT + 1
         add esi, [edi]
         mov ecx, MAP_WIDTH + 1
         imul ecx, [edi+4]
         add esi, ecx
+        ; End updating player
 
-        ;;;; CHECKING WIN/LOSE CONDITIONS
+        ; Check win/lose conditions
+        ; Case step on enemy
         cmp [esi], byte ptr 45h
         je Lose
+        ; Case step on trap
 		cmp [esi], byte ptr 54h
 		je Lose
+        ; Case step on exit
         cmp [esi], byte ptr 57h
         je Win
-        ;;;;
+        ; End checking
 
         mov byte ptr [esi], 50h
         jmp MainLoop
@@ -436,19 +348,72 @@ main proc C
         add esp, 4
         jmp Quit
 
-    Quit:
-        ;add esp, MAP_WIDTH * MAP_HEIGHT + MAP_HEIGHT + 1
-		;add esp, ENEMY_COUNT*8
-		
-		add esp, EXIT_OFFSET
-
+    Quit:		
+        pop ebx
         pop esi
         pop edi
+
+        add esp, EXIT_OFFSET
+
         mov esp, ebp
         pop ebp
 
         xor eax, eax
         ret
 main endp
+
+; void Init(char* pMap, int* pToInit, int numToInit, char howToInit)
+Init proc
+        push ebp
+        mov ebp, esp
+
+        ; storing registers we're gonna modify
+        push edi
+        push esi
+        push ebx
+
+        ; EDI = pointer to something that we're initializing
+        mov edi, [ebp+16]
+        ; EBX = number of things that we're initializing
+        mov ebx, [ebp+12]
+    InitLoop:
+        ; ESI = pointer to the start of the map
+        mov esi, [ebp+20]
+        call rand
+        mov ecx, MAP_WIDTH
+        xor edx, edx
+        idiv ecx
+        mov [edi], edx
+        call rand
+        mov ecx, MAP_HEIGHT
+        xor edx, edx
+        idiv ecx
+        mov [edi+4], edx
+
+        ; Checking if space we're trying to occupy is empty
+        add esi, [edi]
+        mov ecx, MAP_WIDTH + 1
+        imul ecx, [edi+4]
+        add esi, ecx
+        cmp [esi], byte ptr 2eh
+        jne InitLoop
+        mov dl, byte ptr [ebp+8]
+        mov byte ptr [esi], dl
+        ;
+
+        add edi, 8
+        dec ebx
+        cmp ebx, 0
+        jg InitLoop
+
+        ; Restoring registers
+        pop ebx
+        pop esi
+        pop edi
+
+        mov esp, ebp
+        pop ebp
+        ret 16
+Init endp
 
 END
